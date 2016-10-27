3796a8d06f38724be4f96cacf292a65fca134aa7
stjohni933 <stjohni933@strose.edu>
2016-10-26 23:42:01 -0400

    i think i got .5 things done. yay.

--- gradschools.js
+++ gradschools.js
@@ -1,7 +1,55 @@
 /**
  * Created by Iain on 10/26/2016.
  */
+var school = ''
+
+
 //All functions only available when the document is loaded and ready
 $(document).ready(function() {
 
 });
+
+
+function fillTable() {
+    for(var i = 0; i < length(schools); i++) {
+
+    }
+}
+
+function putSchoolInTable(gs) {
+    $("#schoolName").text(gs.skoolname);
+    $("#state").text(gs.state);
+    $("#appDueDate").text(gs.appDueDate);
+    $("#appFee").text(gs.appFee);
+}
+
+
+function GradSchool(name, state, appDueDate, appFee, program) {
+    this.skoolname = name;
+    this.state = state;
+    this.appDueDate = appDueDate;
+    this.appFee = appFee;
+}
+
+function Program(degree, credits, length, cost, costType) {
+    this.degree = degree;
+    this.credits = credits;
+    this.cost = cost;
+    this.costType = costType;
+    calculateAllCosts(credits, length, cost, costType);
+}
+
+function ProgOp(type, opName) {
+    this.type = type;
+    this.opName = opName;
+}
+
+function ProgReq(reqType, req, reqParam) {
+    this.reqType;
+    this.req = req;
+    this.reqParam = reqParam;
+}
+
+function calculateAllCosts(creds, len, cost, costType) {
+
+}
