21b35a1c5be736c42c3655e9f3e8ec1dde935175
stjohni933 <stjohni933@strose.edu>
2016-10-27 23:08:10 -0400

    more work on the page...getting a better picture of what I need to do, and how to do it.

--- gradschools.js
+++ gradschools.js
@@ -1,14 +1,82 @@
 /**
  * Created by Iain on 10/26/2016.
+ * Simple local web page for Danielle to track her candidate schools for graduate school
  */
-var school = ''
+
+/**** GLOBAL VARIABLES, ACCESSIBLE TO THE WHOLE SCRIPT */
+var schoolForm = $("#schoolForm");
+var schoolTable = $("#schoolTable");
+var btnCancel = $("#btnCancel");
+var candidates = [];
 
 
-//All functions only available when the document is loaded and ready
+//This is the 'main()' method of this script
 $(document).ready(function() {
+    $(document).ondblclick(".editableRow",openEditDialog($(this)));
 
+    if(typeof(Storage) == "undefined") {
+        alert("There is no local storage available in this browser. Exiting");
+        $(document).close();
+    } else if(localStorage.length > 0) {
+        alert("Length of the local storage:" + localStorage.length);
+        loadFromLocal();
+    } else {
+        var response = confirm("It looks like you haven't added anything yet. Would you like to?");
+        if(response == true) {
+            alert(response);
+            //open dialog here
+        }
+    }
+
+    //Populate the table
+    populateTable();
 });
 
+
+function populateTable() {
+    var row = "<tr></tr>";
+    row.class = "editableRow";
+    for(var i = 0; i < candidates.length; i++) {
+        var school = candidates[i];
+        row += $("<td></td>").text(school.schoolName);
+        row += $("<td></td>").text(school.state);
+
+    }
+}
+
+function buildProgramCell(school) {
+    var prog = school.program;
+}
+
+/** This function loads a collection of graduate school candidates from the local storage of the browser */
+function loadFromLocal() {
+
+}
+
+/** Saves all objects as JSON objects, and puts it in the browser's local storage */
+function saveToLocal() {
+
+}
+
+/** Open the form dialog with all empty data, to create an entirely new entry */
+function openCreateDialog() {
+    schoolForm.dialog({
+        title: "Dialog",
+        modal: true,
+        open: function () {
+            schoolForm.dialog("open");
+        }
+    });
+}
+
+/** Open the form dialog for an existing row in the grad school table, for editing or deletion */
+function openEditDialog(row) {
+
+}
+
+function cancelAndClose() {
+    schoolForm.close();
+}
 
 function fillTable() {
     for(var i = 0; i < length(schools); i++) {
@@ -16,19 +84,13 @@ function fillTable() {
     }
 }
 
-function putSchoolInTable(gs) {
-    $("#schoolName").text(gs.skoolname);
-    $("#state").text(gs.state);
-    $("#appDueDate").text(gs.appDueDate);
-    $("#appFee").text(gs.appFee);
-}
 
-
-function GradSchool(name, state, appDueDate, appFee, program) {
-    this.skoolname = name;
+function GradSchool(schoolName, state, appDueDate, appFee, program) {
+    this.schoolName = schoolName;
     this.state = state;
     this.appDueDate = appDueDate;
     this.appFee = appFee;
+    this.program = program;
 }
 
 function Program(degree, credits, length, cost, costType) {
@@ -52,4 +114,8 @@ function ProgReq(reqType, req, reqParam) {
 
 function calculateAllCosts(creds, len, cost, costType) {
 
+}
+
+function selectRow() {
+    openEditDialog($())
 }
